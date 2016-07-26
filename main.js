4b5a8508b691bb3fcd8af925f5f7cccf81d93ca0
Samer Buna <samer.buna@gmail.com>
2016-07-26 10:30:34 -0700

    react 1

--- main.js
+++ main.js
@@ -1,8 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+const books = [
+  { id: 1, title: 'Book 1', price: 2900 },
+  { id: 7, title: 'Book 9', price: 3900 },
+  { id: 99, title: 'Book 14', price: 4900 },
+  { id: 101, title: 'Book 66', price: 1900 }
+];
+
+
+const BookList = () => {
+  return (
+    React.createElement(
+      "ul",
+      { className: "book-list" },
+      books.map(book => {
+        return React.createElement(
+          "li",
+          { key: book.id, className: "one-book" },
+          book.title);
+      })
+    )
+  );
+};
+
 ReactDOM.render(
-  <div>Hello React</div>
+  React.createElement(BookList)
   ,
   document.getElementById("react")
 );
