eccf600505e0f21eb3c98e889678b911e1b64423
Samer Buna <samer.buna@gmail.com>
2016-07-26 14:43:36 -0700

    selected

--- components/cell.js
+++ components/cell.js
@@ -1,12 +1,48 @@
 import React from "react";
 
 class Cell extends React.Component {
+  constructor(props) {
+    super(props);
+
+    // this.guess = this.guess.bind(this);
+  }
+
+  isTarget() {
+    const { targets, r, c } = this.props;
+    return targets.filter(obj => {
+      return obj.r === r && obj.c === c
+    }).length === 1;
+  }
+
+  isSelected() {
+    const { selected, r, c } = this.props;
+    return selected.filter(obj => {
+      return obj.r === r && obj.c === c
+    }).length === 1;
+  }
+
+  guess = () => {
+    this.props.selectCell(this.props.r, this.props.c)
+    const isCorrectGuess = this.isTarget();
+  };
+
+  cellClassName() {
+    return `cell ${this.isTarget() ? 'target' : ''}`;
+  }
+
   render() {
     return (
-      <div className="cell">
-        Cell
-       </div>
+      <div className={this.cellClassName()}>
+           onClick={this.guess}>
+           {this.isSelected() ? '1': '0'}
+      </div>
     );
+  }
+
+  static propTypes = {
+    r: React.PropTypes.number,
+    c: React.PropTypes.number,
+    selectCell: React.PropTypes.func
   }
 }
 
--- components/game.js
+++ components/game.js
@@ -4,17 +4,41 @@ import Row from "./row";
 import Cell from "./cell";
 
 class Game extends React.Component {
-  render() {
+  constructor(props) {
+    super(props);
+    this.state = {
+      targets: [
+        { r: 0, c: 2 },
+        { r: 4, c: 4 },
+        { r: 3, c: 0 }
+      ],
+      selected: [
+        { r: 0, c: 2 },
+        { r: 4, c: 4 },
+        { r: 1, c: 0 }
+      ]
+    };
+  }
 
+  selectCell = (r, c) => {
+    this.setState({ selected: this.state.selected.concat({ r, c }) });
+  };
+
+  render() {
     let grid = [], row;
 
     for(let r = 0; r < this.props.rows; r++) {
       row = [];
       for(let c = 0; c < this.props.cols; c++) {
-        row.push(<Cell />);
+        const cellId = `r${r}-c${c}`;
+        row.push(<Cell key={cellId}
+          r={r} c={c}
+          selectCell={this.selectCell}
+          selected={this.state.selected}
+          targets={this.state.targets} />);
       }
       grid.push(
-        <Row>
+        <Row key={r}>
           {row}
         </Row>
       );
