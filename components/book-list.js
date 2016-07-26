6f05a9969e5710c5864881706dfc15444f3731f6
Samer Buna <samer.buna@gmail.com>
2016-07-26 11:53:48 -0700

    extends React.Component

--- components/book-list.js
+++ components/book-list.js
@@ -1,15 +1,35 @@
 import React from "react";
 import Book from "./book";
 
-const BookList = ({ books }) => {
-  return (
-    <ul className="book-list">
-      {books.map(book => {
-        return <Book key={book.id} {...book} />
+// Stateless function component
+// const BookList = ({ books }) => {
+//   const zebraClass = index => index % 2 ? 'even' : 'odd';
+//
+//   return (
+//     <ul className="book-list">
+//       {books.map((book, index) => {
+//         return <Book key={book.id} zebraClass={zebraClass(index)} {...book} />
+//       })}
+//     </ul>
+//   );
+// };
+
+class BookList extends React.Component {
+  zebraClass(index) {
+    return index % 2 ? 'even' : 'odd';
+  }
+  render() {
+    const { books } = this.props;
+    return (
+      <ul className="book-list">
+      {books.map((book, index) => {
+        return <Book key={book.id} zebraClass={this.zebraClass(index)} {...book} />
       })}
-    </ul>
-  );
-};
+      </ul>
+    );
+  }
+}
+
 
 BookList.propTypes = {
   books: React.PropTypes.arrayOf(React.PropTypes.object)
--- components/book.js
+++ components/book.js
@@ -2,15 +2,16 @@ import React from "react";
 
 import { convertToUSD } from "../util";
 
-const Book = ({ title, price }) => {
+const Book = ({ title, price, zebraClass }) => {
   return (
-    <li className="one-book">
+    <li className={`one-book ${zebraClass}`}>
       {title}, {convertToUSD(price)}
     </li>
   );
 };
 
 Book.propTypes = {
+  zebraClass: React.PropTypes.string.isRequired,
   title: React.PropTypes.string.isRequired,
   price: React.PropTypes.number.isRequired
 };
