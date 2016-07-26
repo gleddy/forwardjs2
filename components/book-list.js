ca169dd3639281c81294a97f96ac9f420370d05b
Samer Buna <samer.buna@gmail.com>
2016-07-26 13:19:47 -0700

    cleanup

--- components/book-list.js
+++ components/book-list.js
@@ -15,14 +15,39 @@ import Book from "./book";
 // };
 
 class BookList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { books: this.props.books }
+  }
   zebraClass(index) {
     return index % 2 ? 'even' : 'odd';
   }
+  componentWillMount() {
+    debugger;
+  }
+  componentDidMount() {
+    debugger;
+  }
+  componentWillUnmount() {
+    debugger;    
+  }
+  componentWillRecieveProps() {
+
+  }
+  componentWillUpdate() {
+
+  }
+  componentDidUpdate(prevProps, prevState) {
+
+  }
+  shouldComponentUpdate() {
+
+  }
+
   render() {
-    const { books } = this.props;
     return (
       <ul className="book-list">
-      {books.map((book, index) => {
+      {this.state.books.map((book, index) => {
         return <Book key={book.id} zebraClass={this.zebraClass(index)} {...book} />
       })}
       </ul>
--- main.js
+++ main.js
@@ -1,17 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const books = [
-  { id: 1, title: 'Book 1', price: 2900 },
-  { id: 7, title: 'Book 9', price: 3900 },
-  { id: 99, title: 'Book 14', price: 4900 },
-  { id: 101, title: 'Book 66', price: 1900 }
-];
-
-import BookList from "./components/book-list";
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        Game here...
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(
-  <BookList books={books} />
+  <App />
   ,
   document.getElementById("react")
 );
